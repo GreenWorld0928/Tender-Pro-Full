1810044a76e530885c0e1b233a23f008f5f9a5bd
Green World <green.world928@outlook.com>
2024-04-19 18:30:57 -0500

    Added postgresql db option

--- backend/Tender_Pro_Django/settings.py
+++ backend/Tender_Pro_Django/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Tender_Pro_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Tender_Pro_Db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
